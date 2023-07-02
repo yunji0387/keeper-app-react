d6936cd86e8c7a9bd8743288e34a172b12e7cf65
yunji0387 <yunji0387@gmail.com>
2023-07-02 14:11:27 -0500

    Reintialized based files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<App />);
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+//6. Make sure that the final website is styled like the example shown here:
+//https://w00gz.csb.app/
+
+//HINT: You will need to study the classes in teh styles.css file to appy styling.
